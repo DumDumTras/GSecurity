5dc8254d31a7d8dec31f63f53693a4440aa5cae1
DumDumTras <znastidobrostoje@gmail.com>
2026-01-11 02:36:29 +0100

    Update GSecurity.bat

--- Iso/sources/$OEM$/$$/Setup/Scripts/Bin/GSecurity.bat
+++ Iso/sources/$OEM$/$$/Setup/Scripts/Bin/GSecurity.bat
@@ -46,10 +46,10 @@ takeown /f %windir%\system32\consent.exe /A
 icacls %windir%\system32\consent.exe /reset
 icacls %windir%\system32\consent.exe /inheritance:r
 icacls %windir%\system32\consent.exe /grant:r "Console Logon":RX
-takeown /f %windir%\System32\Oobe\winmm.dll /A
-icacls "%windir%\System32\Oobe\winmm.dll" /reset
-icacls "%windir%\System32\Oobe\winmm.dll" /inheritance:r
-icacls "%windir%\System32\Oobe\winmm.dll" /grant:r "Console Logon":RX
+takeown /f "%windir%\System32\winmm.dll" /A
+icacls "%windir%\System32\winmm.dll" /reset
+icacls "%windir%\System32\winmm.dll" /inheritance:r
+icacls "%windir%\System32\winmm.dll" /grant:r "Console Logon":RX
 Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d "5" /f
 Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorUser" /t REG_DWORD /d "1" /f
 
@@ -95,4 +95,5 @@ sc stop SNMP
 
 :: Restart
 shutdown /r /t 0
+
 
